b1d478c7e6eb74f26569edfca8c358c9f90d978a
Paul Tsouchlos <developer.paul.123@gmail.com>
2026-02-12 23:27:02 -0500

    fix: minor build issues
    bench: 1025548

--- engine/src/history_table.rs
+++ engine/src/history_table.rs
@@ -3,11 +3,7 @@
 // GNU General Public License v3.0 or later
 // https://www.gnu.org/licenses/gpl-3.0-standalone.html
 
-use chess::{
-    definitions::NumberOf,
-    pieces::{PIECE_NAMES, Piece},
-    side::Side,
-};
+use chess::{definitions::NumberOf, pieces::PIECE_NAMES, side::Side, square::Square};
 
 use crate::score::{LargeScoreType, Score};
 
