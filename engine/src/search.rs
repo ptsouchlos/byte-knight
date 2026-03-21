d46fc4f492db77aa62db34fecee1d30ee4e2dca2
Paul Tsouchlos <developer.paul.123@gmail.com>
2026-03-21 08:02:36 -0400

    fix: is_quiet detection in search
    Was accidentally not excluduing promotions from quiet detection.
    
    bench: 720244

--- engine/src/search.rs
+++ engine/src/search.rs
@@ -562,7 +562,7 @@ impl<'a, Log: LogLevel> Search<'a, Log> {
             let is_in_check = move_generation::is_in_check(board);
             let is_root = Node::ROOT;
             let is_pv = Node::PV;
-            let is_quiet = board.piece_on_square(mv.to()).is_none() & !mv.is_en_passant_capture();
+            let is_quiet = board.piece_on_square(mv.to()).is_none() && !mv.is_en_passant_capture() && !mv.is_promotion();
             let piece = board.piece_on_square(mv.from()).map(|(pc, _)| pc).unwrap();
 
             // Move-loop pruning techniques
@@ -666,7 +666,7 @@ impl<'a, Log: LogLevel> Search<'a, Log> {
                         for mv in move_list
                             .iter()
                             .take(loop_counter)
-                            .filter(|mv| board.captured(mv).is_none())
+                            .filter(|mv| board.captured(mv).is_none() && !mv.is_promotion())
                         {
                             let piece = board.piece_on_square(mv.from()).map(|(pc, _)| pc).unwrap();
                             self.history_table.update(
