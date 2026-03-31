e736703f036e231cead9a3a960c1c3cd5fd5eaae
Paul Tsouchlos <developer.paul.123@gmail.com>
2026-03-31 14:09:44 -0400

    chore(search): skip quiets for LMP instead of break
    bench: 1171667

--- engine/src/search.rs
+++ engine/src/search.rs
@@ -561,7 +561,8 @@ impl<'a, Log: LogLevel> Search<'a, Log> {
                 && depth <= lmp_max_depth() as i16
                 && moves_seen > params::late_move_threshold(depth as i32)
             {
-                break;
+                // Skip quiets
+                picker.skip_quiets = true;
             }
 
             // local PV is for each node below this one is different when we call negamax recursively
