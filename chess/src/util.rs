f21dd6e13ff3c7b4d4ea7771c59b89b80918c927
Paul Tsouchlos <developer.paul.123@gmail.com>
2026-04-14 11:53:03 -0400

    chore: inline ArrayVecList functions
    bench: 845875

--- chess/src/util.rs
+++ chess/src/util.rs
@@ -18,6 +18,7 @@ impl<T, const N: usize> Default for ArrayVecList<T, N> {
 
 impl<T, const N: usize> ArrayVecList<T, N> {
     /// Create a new, empty list.
+    #[inline]
     pub fn new() -> Self {
         Self {
             items: ArrayVec::new(),
@@ -25,39 +26,49 @@ impl<T, const N: usize> ArrayVecList<T, N> {
     }
 
     /// Returns the number of items in the list.
+    #[inline]
     pub fn len(&self) -> usize {
         self.items.len()
     }
 
     /// Returns true if the list is empty.
+    #[inline]
     pub fn is_empty(&self) -> bool {
         self.items.is_empty()
     }
 
     /// Push an item to the list. Panics if the list is full.
+    #[inline]
     pub fn push(&mut self, item: T) {
         self.items.push(item);
     }
 
     /// Get an iterator over the items in the list.
+    #[inline]
     pub fn iter(&self) -> impl Iterator<Item = &T> {
         self.items.iter()
     }
 
     /// Get the item at the given index. Returns `None` if out of bounds.
+    #[inline]
     pub fn at(&self, index: usize) -> Option<&T> {
         self.items.get(index)
     }
 
     /// Clear the list.
+    #[inline]
     pub fn clear(&mut self) {
         self.items.clear();
     }
 
+    /// Return the list as a slice.
+    #[inline]
     pub fn as_slice(&self) -> &[T] {
         self.items.as_slice()
     }
 
+    /// Return the list as a mutable slice.
+    #[inline]
     pub fn as_mut_slice(&mut self) -> &mut [T] {
         self.items.as_mut_slice()
     }
