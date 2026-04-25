51339ac711bd602b7cb75b5ca2ec3a0ccc40e4c2
Paul T <6591180+ptsouchlos@users.noreply.github.com>
2026-04-25 19:31:59 -0400

    feat: check extensions (#324)
    Extend search depth when in check (#239)
    
    STC
    
    Elo   | 40.89 +- 13.10 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 3.03 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 1340 W: 472 L: 315 D: 553
    Penta | [26, 118, 262, 201, 63]
    https://openbench.nocturn9x.space/test/6701/
    
    bench: 1396105

--- engine/src/search.rs
+++ engine/src/search.rs
@@ -404,6 +404,7 @@ impl<'a, Log: LogLevel> Search<'a, Log> {
         // increment node count
         self.thread_data.nodes += 1;
         self.thread_data.seldepth = self.thread_data.seldepth.max(ply);
+        let in_check = move_generation::is_in_check(board);
 
         // Ply guard: prevent unbounded recursion
         if ply >= MAX_PLY {
@@ -472,6 +473,13 @@ impl<'a, Log: LogLevel> Search<'a, Log> {
         let mut best_move: Option<Move> = None;
         let mut moves_seen = 0;
         let static_eval = self.eval.eval(board);
+
+        // How much to extend the depth.
+        let mut extension = 0;
+        // Check extension - if we're in check, this is likely a tactical position so search deeper.
+        if in_check && extension == 0 {
+            extension = 1;
+        }
 
         let fp_margin = fp_base() + depth as i32 * fp_scale();
 
@@ -580,6 +588,8 @@ impl<'a, Log: LogLevel> Search<'a, Log> {
                     1
                 };
 
+                let new_depth = depth - 1 + extension;
+
                 // -----------------------------------------------------------------------
                 // Principal Variation Search (PVS)
                 // We make the assumption that the first move will be best.
@@ -589,9 +599,9 @@ impl<'a, Log: LogLevel> Search<'a, Log> {
                 if moves_seen > 0 {
                     // Figure out the search depth, factoring in the LMR reduction.
                     let search_depth = if reduction > 1 {
-                        depth.saturating_sub(reduction)
+                        new_depth.saturating_sub(reduction)
                     } else {
-                        depth - 1
+                        new_depth
                     };
                     score = -self.negamax::<NonPvNode>(
                         board,
@@ -606,7 +616,7 @@ impl<'a, Log: LogLevel> Search<'a, Log> {
                     if score > alpha && reduction > 1 {
                         score = -self.negamax::<NonPvNode>(
                             board,
-                            depth - 1,
+                            new_depth,
                             ply + 1,
                             -alpha - 1,
                             -alpha,
@@ -618,7 +628,7 @@ impl<'a, Log: LogLevel> Search<'a, Log> {
                 else if !Node::PV {
                     score = -self.negamax::<NonPvNode>(
                         board,
-                        depth - 1,
+                        new_depth,
                         ply + 1,
                         -alpha - 1,
                         -alpha,
@@ -631,7 +641,7 @@ impl<'a, Log: LogLevel> Search<'a, Log> {
                 if Node::PV && (moves_seen == 0 || (score > alpha && score < beta)) {
                     score = -self.negamax::<Node::Next>(
                         board,
-                        depth - 1,
+                        new_depth,
                         ply + 1,
                         -beta,
                         -alpha,
