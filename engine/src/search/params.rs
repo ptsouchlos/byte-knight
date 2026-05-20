80168fbb9faff48ec091220cb375821b51f1c4f9
Paul Tsouchlos <developer.paul.123@gmail.com>
2026-05-20 11:09:08 -0400

    chore: add params for lmp improvement factors
    bench: 1345914

--- engine/src/search/params.rs
+++ engine/src/search/params.rs
@@ -1,5 +1,9 @@
-use crate::tuneable::lmp_base;
+use crate::tuneable::{
+    lmp_base, lmp_improvement_divisor, lmp_improvement_max, lmp_improvement_min,
+};
 #[inline]
-pub(crate) fn late_move_threshold(depth: i32) -> i32 {
-    lmp_base() + depth * depth
+pub(crate) fn late_move_threshold(depth: i32, improvement: i32) -> i32 {
+    let scaled_improvement = improvement / lmp_improvement_divisor();
+    let adjustment = scaled_improvement.clamp(lmp_improvement_min(), lmp_improvement_max());
+    lmp_base() + depth * depth + adjustment
 }
