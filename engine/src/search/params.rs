1758c696ef3288109f8eb196072a1995bfe2a2d2
Paul Tsouchlos <developer.paul.123@gmail.com>
2026-05-21 13:11:42 -0400

    feat: adjust NMP based on if eval is improving
    bench: 1191392

--- engine/src/search/params.rs
+++ engine/src/search/params.rs
@@ -1,6 +1,13 @@
 use crate::tuneable::{
     lmp_base, lmp_improvement_divisor, lmp_improvement_max, lmp_improvement_min,
+    nmp_depth_reduction, nmp_improving_bonus,
 };
+
+#[inline]
+pub(crate) fn nmp_reduction(improving: bool) -> i32 {
+    nmp_depth_reduction() + if improving { nmp_improving_bonus() } else { 0 }
+}
+
 #[inline]
 pub(crate) fn late_move_threshold(depth: i32, improvement: i32) -> i32 {
     let clamped_improvement = improvement.clamp(lmp_improvement_min(), lmp_improvement_max());
