b82173f8880c08c474381f4ac9a07d47ed349f45
Paul Tsouchlos <developer.paul.123@gmail.com>
2026-06-17 13:56:54 -0400

    wip: refactor zobrist module
    bench: 1287259

--- chess/src/zobrist.rs
+++ chess/src/zobrist.rs
@@ -3,12 +3,30 @@
 // GNU General Public License v3.0 or later
 // https://www.gnu.org/licenses/gpl-3.0-standalone.html
 
+pub mod keys;
+pub mod values;
+
 use rand::{Rng, SeedableRng, rngs::StdRng};
 
-use crate::definitions::NumberOf;
+use crate::{board::Board, definitions::NumberOf};
 
 /// A Zobrist hash value.
 pub type ZobristHash = u64;
+
+#[derive(Debug, Default)]
+struct Hashes {
+    board: u64,
+    pawn: u64,
+}
+
+impl Hashes {
+    pub fn new(board: &Board) -> Self {
+        Hashes {
+            board: keys::get_hash(board),
+            pawn: keys::get_pawn_hash(board),
+        }
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 pub(crate) struct ZobristRandomValues {
@@ -87,5 +105,56 @@ impl ZobristRandomValues {
     /// Returns the Zobrist hash value for the given side.
     pub fn get_side_value(&self, side: usize) -> u64 {
         self.side_values[side]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{definitions::NumberOf, pieces::Piece, side::Side, zobrist::ZobristRandomValues};
+
+    #[test]
+    fn print_params() {
+        let values = ZobristRandomValues::default();
+        println!(
+            "const PIECE_VALUES: [[[u64; NumberOf::SQUARES]; NumberOf::PIECE_TYPES]; NumberOf::SIDES] = ["
+        );
+        for side in Side::iter() {
+            println!("  // {side}");
+            println!("  [");
+            for piece in Piece::iter() {
+                println!("    // {piece}");
+                println!("    [");
+                for sq in 0..NumberOf::SQUARES {
+                    print!(
+                        "{}, ",
+                        values.get_piece_value(piece as usize, side as usize, sq)
+                    );
+                }
+                println!("    ],");
+            }
+            println!("  ],");
+        }
+        println!("];");
+
+        println!("const CASTLING_VALUES: [u64; NumberOf::CASTLING_OPTIONS] = [");
+        println!("  ");
+        for castling in 0..NumberOf::CASTLING_OPTIONS {
+            print!("{}, ", values.get_castling_value(castling));
+        }
+        println!("];");
+
+        println!("const EN_PASSANT_VALUES: [u64; NumberOf::SQUARES + 1] = [");
+        println!("   ");
+        for ep in 0..NumberOf::SQUARES + 1 {
+            print!("{}, ", values.get_en_passant_value(Some(ep as u8)));
+        }
+        println!("];");
+
+        println!("const SIDE_VALUES: [u64; NumberOf::SIDES] = [");
+        for sd in 0..NumberOf::SIDES {
+            print!("{}, ", values.get_side_value(sd));
+        }
+        println!();
+        println!("];");
     }
 }
