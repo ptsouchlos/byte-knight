b1ae94b672ae48b38285e3924d1d5934e370e063
Paul Tsouchlos <developer.paul.123@gmail.com>
2026-08-06 12:19:26 -0400

    chore: add cont hist clear test
    Added a small test to make sure we don't blow up the stack when clearing
    the continuation history table.
    
    bench: 1267855

--- crates/engine/src/history/continuation_history.rs
+++ crates/engine/src/history/continuation_history.rs
@@ -8,7 +8,7 @@ use chess::{moves::Move, pieces::Piece};
 use crate::{
     history::{types::PieceToHistory, util::gravity},
     score::LargeScoreType,
-    utils::{self},
+    utils::{self, boxed_and_zeroed},
 };
 
 pub struct ContinuationHistory {
@@ -38,7 +38,7 @@ impl ContinuationHistory {
     }
 
     pub(crate) fn clear(&mut self) {
-        self.single_ply_entries = unsafe { utils::boxed_and_zeroed() };
+        self.single_ply_entries = unsafe { boxed_and_zeroed() };
     }
 }
 
@@ -47,5 +47,37 @@ impl Default for ContinuationHistory {
         Self {
             single_ply_entries: unsafe { utils::boxed_and_zeroed() },
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chess::{
+        moves::{Move, MoveFlag},
+        pieces::Piece,
+        square::Square,
+    };
+
+    use crate::history::continuation_history::ContinuationHistory;
+
+    #[test]
+    fn clear_table() {
+        // This test is mostly here to validate that we don't overflow the stack when clearing the table.
+        let mut cont_hist = ContinuationHistory::default();
+        let prev_mv = Move::new(Square::B2, Square::B4, MoveFlag::DoublePush);
+        let mv = Move::new(Square::B4, Square::B5, MoveFlag::Standard);
+        let bonus = 300;
+        let pc = Piece::Pawn;
+        // Update the score
+        cont_hist.update(prev_mv, pc, mv, pc, bonus);
+        // Ensure it's non-zero
+        let score = cont_hist.get(prev_mv, pc, mv, pc);
+        assert!(score > 0);
+
+        // Clear the table
+        cont_hist.clear();
+        // Now the score should be 0
+        let score = cont_hist.get(prev_mv, pc, mv, pc);
+        assert!(score == 0);
     }
 }
