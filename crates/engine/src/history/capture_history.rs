c12d5a318b6db0b1d2141b519ce5024f8aeecc90
Paul Tsouchlos <developer.paul.123@gmail.com>
2026-09-16 22:53:39 -0400

    feat: add capture history implementation
    Initial cap hist implementation.

---
+++ crates/engine/src/history/capture_history.rs
@@ -0,0 +1,53 @@
+// Part of the byte-knight project.
+// Author: Paul Tsouchlos (ptsouchlos) (developer.paul.123@gmail.com)
+// GNU General Public License v3.0 or later
+// https://www.gnu.org/licenses/gpl-3.0-standalone.html
+
+//! Contains capture history table for tracking successful captures.
+
+use chess::{board::Board, moves::Move, pieces::Piece};
+
+use crate::{
+    history::{types::PieceToHistory, util},
+    score::LargeScoreType,
+    utils,
+};
+
+/// Capture history table containing [attacker piece][to][victim piece] per side.
+pub struct CaptureHistory {
+    piece_to_entries: Box<[PieceToHistory<[i16; Piece::COUNT]>; 2]>,
+}
+
+impl CaptureHistory {
+    const MAX: i32 = 16384;
+    const BONUS_MAX: i32 = Self::MAX / 4;
+
+    pub(crate) fn get(&self, board: &Board, mv: Move, pc: Piece, captured_piece: Piece) -> i16 {
+        self.piece_to_entries[board.side_to_move()][pc][mv.to()][captured_piece]
+    }
+
+    pub(crate) fn update(
+        &mut self,
+        board: &Board,
+        mv: Move,
+        pc: Piece,
+        captured_piece: Piece,
+        bonus: LargeScoreType,
+    ) {
+        let bonus = bonus.clamp(-Self::BONUS_MAX, Self::BONUS_MAX);
+        let value = &mut self.piece_to_entries[board.side_to_move()][pc][mv.to()][captured_piece];
+        *value = util::gravity(*value as i32, bonus, Self::MAX) as i16;
+    }
+
+    pub(crate) fn clear(&mut self) {
+        self.piece_to_entries = unsafe { utils::boxed_and_zeroed() }
+    }
+}
+
+impl Default for CaptureHistory {
+    fn default() -> Self {
+        Self {
+            piece_to_entries: unsafe { utils::boxed_and_zeroed() },
+        }
+    }
+}
